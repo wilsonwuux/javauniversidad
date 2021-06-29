7f4d6db1451a5118d164bace5535eb032b3fa079
https://wilsonwuux@github.com <wilsonwuux@gmail.com>
2021-06-28 23:16:00 -0600

    se agregaron tipos de datos

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,36 @@
 class HolaMundo{
     // metodo principal 
     public static void main(String[] args) {
-
+        //impresion en consola
         System.out.println("hola Mundo");
-    }
+        // Tipos de Dato primitivos enteros, byte, short , int, long
+        // long	8	–9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+        // int	4	–2,147,483,648 a 2,147,483,647
+        // short	2	–32,768 to 32,767
+        // byte	1	–128 to 127
+        byte diasMes = 31;
+        short diasLustros =(12*31)*5;//por 5 años
+        int velocidadLuz=299792458;
+        long yearsLuz=velocidadLuz *365;
 
+        System.out.println("esto es un byte");
+        System.out.println(diasMes);
+        System.out.println("esto es un short");
+        System.out.println(diasLustros);
+        System.out.println("esto es un int");
+        System.out.println(velocidadLuz);
+        System.out.println("esto es un long");
+        System.out.println(yearsLuz);
+
+        // Tipos de Dato primitivos flotantes float double
+        // double	8	4.9e–324 to 1.8e+308
+        // float	4	1.4e–045 to 3.4e+038
+        float pi = 3.1415926535f;
+        double e = 2.718281828459045235360;
+            System.out.println("esto es un float ");
+            System.out.println(pi);
+            System.out.println("esto es un double ");
+            System.out.println(e);
+
+    }
 }
